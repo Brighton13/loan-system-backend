98255a996c6f9dc392a73b74c669fdeb2eb75dc3
Banda Brighton <brightonbanda13@gmail.com>
2025-07-09 23:41:10 +0300

    jk

--- dist/controllers/payementController.d.ts
+++
@@ -1,8 +0,0 @@
-import { Request, Response } from 'express';
-import User from '../models/User';
-interface AuthRequest extends Request {
-    user?: User;
-}
-export declare const make_payment: (req: AuthRequest, res: Response) => Promise<Response<any, Record<string, any>>>;
-export {};
-//# sourceMappingURL=payementController.d.ts.map
--- dist/middleware/auth.d.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response, NextFunction } from 'express';
-import User, { UserRole } from '../models/User';
-interface AuthRequest extends Request {
-    user?: User;
-}
-export declare const authenticate: (req: AuthRequest, res: Response, next: NextFunction) => Promise<Response<any, Record<string, any>> | undefined>;
-export declare const authorize: (...roles: UserRole[]) => (req: AuthRequest, res: Response, next: NextFunction) => Response<any, Record<string, any>> | undefined;
-export {};
-//# sourceMappingURL=auth.d.ts.map
